89c494b628e7118111fb3c34b681e5d202edcf70
MostofaFatin <79892218+MostofaFatin@users.noreply.github.com>
2021-03-15 09:23:52 +0600

    Add files via upload

---
+++ Assignment_1/Assignment01/Assignment01/Account.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Assignment01
+
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;//1-1 Relation
+                                //private double amount;
+
+
+        public Account(string accountName, double balance, int acn, Address address)
+        {
+            this.accountName = accountName;
+            this.balance = balance;
+            this.address = address;
+            this.accountNumber = acn;
+        }
+
+
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = value; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (this.balance >= amount)
+            {
+                this.balance = this.balance - amount;
+                Console.WriteLine("Withdaw Successful");
+            }
+            else
+            {
+                Console.WriteLine("Withdaw Not possible");
+            }
+            // return balance;
+
+
+        }
+
+        //public void Transfer(Account receiver,double amount)
+        public void Transfer(Account receiver, double amount)
+        {
+            receiver.Deposite(amount);
+            this.Withdraw(amount);
+            Console.WriteLine("Transfer Successfully Completed");
+
+        }
+
+        public void Deposite(double amount)
+        {
+
+            this.balance = this.balance + amount;
+            Console.WriteLine("Depisited!!");
+
+
+        }
+
+        public void PrintAccount()
+        {
+
+            Console.WriteLine("\nAccount No:{0}\nAccount Name:{1}\nBalance:{2}", this.accountNumber, this.accountName, this.balance);
+            this.address.PrintAddress();
+        }
+
+
+
+    }
+}
---
+++ Assignment_1/Assignment01/Assignment01/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+
+{
+    class Program
+    {
+
+        static int AccountNumberGen(int inc)
+        {
+            int start = 999;
+            int an = start + inc;
+            return an;
+        }
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int num;
+
+            //Console.WriteLine("How many accounts do you want to create? ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //Bank ourBank = new Bank("Developer's bank", num);
+
+            // Bank ourBank = new Bank("Developer's bank", num);
+
+            Console.WriteLine("How many accounts do you want to create? ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Bank ourBank = new Bank("Developer's bank", num);
+
+            while (flag)
+            {
+                Console.WriteLine("1) Create Account \n2) Deposit \n3) Withdraw \n4) Transfer \n5) Delete \n6) Print \n7) Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+
+
+                        for (int i = 0; i < num; i++)
+                        {
+                            Console.WriteLine("\nAccount " + (i + 1));
+                            Console.WriteLine("Name: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Ammount: ");
+                            double ammount = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("House no: ");
+                            int houseNo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Road no: ");
+                            int roadNo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("City: ");
+                            string city = Console.ReadLine();
+                            Console.WriteLine("Country: ");
+                            string country = Console.ReadLine();
+
+                            ourBank.AddAccount(new Account(name, ammount, AccountNumberGen(i), new Address(houseNo, roadNo, city, country)));
+
+                        }
+                        break;
+                    case 2:
+                        ourBank.Transaction(2, 100.0);
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        Console.WriteLine("House no: ");
+                        int delAC = Convert.ToInt32(Console.ReadLine());
+                        ourBank.DeleteAccount(delAC);
+                        break;
+                    case 6:
+                        ourBank.PrintAllAccounts();
+                        break;
+                    case 7:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("\nPlease Select between 1 to 7\n");
+                        continue;
+
+
+
+                }
+
+
+
+
+
+
+
+
+
+                //Bank ourBank = new Bank("Developer's bank", 5);
+                //ourBank.AddAccount(new Account( "Shakib", 2000, new Address(45, 10, "Dhaka","BD")));
+                // ourBank.AddAccount(new Account("Mushfiq", 4000, new Address(45, 10, "Sylhet", "BD")));
+                // ourBank.AddAccount(new Account("Tamim", 3000, new Address(5, 7, "Chittagong", "BD")));
+                // ourBank.PrintAllAccounts();
+
+            }
+
+
+
+        }
+
+
+
+        /* Address address1 = new Address(45,10,"Dhaka","Bangladesh");
+         address1.PrintAddress();
+         Account account1 = new Account("Shakib", 2000, address1);
+         account1.PrintAccount();
+         Account account2 = new Account("Shakib", 2000, address1);
+         account1.PrintAccount();
+        */
+
+        //Console.ReadKey();
+
+
+    }
+}
